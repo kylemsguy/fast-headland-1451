a8beaa794bab0c80df4624dd7ed5fa70b6e95615
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-04 19:03:20 -0700

    file upload works now

--- hello.py
+++ hello.py
@@ -2,10 +2,13 @@
 from __future__ import division, absolute_import
 
 import os
+import uuid
 from flask import Flask
 from flask import request
 from flask import render_template
 from flask import session
+from flask import redirect
+from flask import url_for
 
 from secret_key import install_secret_key
 
@@ -23,9 +26,17 @@ def hello():
 @app.route('/upload/', methods=['GET', 'POST'])
 def upload_apk():
     if request.method == "POST":
+        unique_filename = uuid.uuid4().urn[9:] + '.apk'
+        f = request.files['File']
+        f.save('tmp/' + unique_filename)
+        session['last_file'] = unique_filename
+        return render_template('file_uploaded.html', filename=unique_filename)
+    else:
+        return redirect('/', code=302)
 
-        f = request.files['the_file']
-        f.save('tmp/')
+@app.route('/getfile/')
+def get_uploaded_apk():
+    return "Not Implemented"
 
 if __name__ == "__main__":
     app.run()
